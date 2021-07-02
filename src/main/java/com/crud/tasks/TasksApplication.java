5764b1daaa7c0c9758d2f64fd96db62c94403127
Monika Borkowska <monikademianiuk@gmail.com>
2021-07-02 19:49:30 +0200

    Task 22.2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
-//
+
 //    @Override
 //    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //       return application.sources(TasksApplication.class);
