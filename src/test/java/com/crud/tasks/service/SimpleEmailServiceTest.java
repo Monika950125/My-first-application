d2ecbf3a6c0f8603abe529ab877fecee7da20220
Monika Borkowska <monikademianiuk@gmail.com>
2021-09-18 20:32:09 +0200

    Task 32.4

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -8,10 +8,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 
+import javax.mail.internet.MimeMessage;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -23,6 +26,7 @@ class SimpleEmailServiceTest {
     @Mock
     private JavaMailSender javaMailSender;
 
+
     @Test
     public void shouldSendEmail() {
         //Given
@@ -30,6 +34,7 @@ class SimpleEmailServiceTest {
         Mail mail = new Mail("test@test.com","test", "Test Message", null);
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
+
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
@@ -39,7 +44,7 @@ class SimpleEmailServiceTest {
         simpleEmailService.send(mail);
 
         //Then
-        verify(javaMailSender, times(1)).send(mailMessage);
+        verify(javaMailSender, times(1)).send(any(MimeMessagePreparator.class));
     }
 
 
